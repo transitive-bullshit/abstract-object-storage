f4dc61a6a2e3ee7ed276351f65fb55359fff9aca
Travis Fischer <fisch0920@gmail.com>
2018-04-12 15:13:06 -0400

    bugfix uploadStream; add real test case (non-CI) for local testing

---
+++ real.test.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const fs = require('fs')
+const assert = require('chai').assert
+
+const ObjectStorage = require('.')
+
+if (!process.env.CI) {
+  describe('object-storage real usage', () => {
+    it('upload and download should work for realz', async () => {
+      const storage = ObjectStorage.defaults({
+        bucket: 'automagical-media-prod'
+      })
+
+      await storage.uploadFile({
+        key: 'test.json',
+        localPath: './package.json',
+        options: {
+          public: true
+        }
+      })
+
+      const buffer = await storage.downloadBuffer({
+        key: 'test.json'
+      })
+      const result = buffer.toString('utf8')
+
+      assert.deepEqual(result, fs.readFileSync('./package.json', 'utf8'))
+    }).timeout(10000)
+  })
+}
